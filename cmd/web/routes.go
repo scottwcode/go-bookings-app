a39f0e53b1ac47c7cc17cb579694b04e03d923b4
scottwcode <scottmail77@gmail.com>
2022-12-01 12:36:36 -0500

    Added own middleware messages and added nosurf middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,21 +10,27 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// muxer from https://github.com/bmizerany/pat
+	// ** muxer from https://github.com/bmizerany/pat
 	// which is a Sinatra style pattern muxer for Go's net/http library
 	// mux := pat.New()
 
 	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
-	// muxer from https://github.com/go-chi/chi
+	// ** muxer from https://github.com/go-chi/chi
 	// chi is a lightweight, idiomatic and composable router for building
 	// Go HTTP services for connecting to various middleware handlers
 	mux := chi.NewRouter()
 
-	// Use Recoverer middleware handler
+	// ** Use Recoverer middleware handler
 	//    https://pkg.go.dev/github.com/go-chi/chi/middleware#Recoverer
 	mux.Use(middleware.Recoverer)
+	// ** Use our own middleware to write to console
+	// mux.Use(WriteToConsole)
+	// ** Use NoSurf middleware https://github.com/justinas/nosurf
+	// which  is an HTTP package for Go that helps you prevent
+	// Cross-Site Request Forgery attacks.
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
